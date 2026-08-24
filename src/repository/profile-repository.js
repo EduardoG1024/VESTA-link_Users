6b42b1325a149fc602bb6291cb3c66538c1541c6
EduardoG1024 <spawnlol2424@gmail.com>
2026-08-23 18:05:23 -0700

    ADDED profile repository working

---
+++ src/repository/profile-repository.js
@@ -0,0 +1,48 @@
+import { pool } from "../config/database.js";
+
+export class ProfileRepository {
+
+    static async AddNewUserLink(id, url, embed, one, two, three) {
+        try {
+            const query = `INSERT INTO links (user_id, url, embed, category_1, category_2, category_3)
+                           VALUES($1, $2, $3, $4, $5, $6)`;
+            const values = [id, url, embed, one, two, three];
+            const result = await pool.query(query, values);
+
+            return result;
+        } catch (error) {
+            throw new Error('Error al añadir nueva URL en la DB');
+        }
+    }
+    
+    static async UpdateExistingUserLink(url, embed, one, two, three, id) {
+        try {
+            const query = `UPDATE links SET 
+                           url = $1
+                           embed = $2
+                           category_1 = $3
+                           category_2 = $4
+                           category_3 = $5
+                           WHERE user_id = $6`;
+            const values = [url, embed, one, two, three, id];
+            const result = await pool.query(query, values);
+
+            return result;
+        } catch (error) {
+            throw new Error('Error al actualizar la URL en la DB');
+        }
+    }
+
+    static async DeleteUserSingleLink(id, url) {
+        try {
+            const query = `DELETE FROM links
+                           WHERE user_id = $1 AND url = $2`;
+            const values = [id, url];
+            const result = await pool.query(query, values);
+
+            return result;
+        } catch (error) {
+            throw new Error('Error al eliminar URL en la DB');
+        }
+    }
+}
